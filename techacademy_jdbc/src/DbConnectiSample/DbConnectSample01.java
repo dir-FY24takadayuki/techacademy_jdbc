ad9f0809a136b223642cd9b937eb21799eae0007
YukiTakada(髙田雄樹) <yuki.takada.s523@dirbato.co.jp>
2024-04-17 16:19:36 +0900

    lesson10全部

--- techacademy_jdbc/src/DbConnectiSample/DbConnectSample01.java
+++ techacademy_jdbc/src/DbConnectiSample/DbConnectSample01.java
@@ -13,34 +13,48 @@ public class DbConnectSample01 {
 
     public static void main(String[] args) {
         // 1. ドライバーのクラスをJava上で読み込む
-        Connection con = null;
-        Statement stmt = null;
-        ResultSet rs = null;
+//        Connection con = null;
+//        Statement stmt = null;
+//        ResultSet rs = null;
         
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             
             // 2. DBと接続する
-            con = DriverManager.getConnection(
-                "jdbc:mysql://localhost/world?useSSL=false&allowPublicKeyRetrieval=true",
-                "root",
-                "141592****yt"
-            );
-            // 3. DBとやりとりする窓口（Statementオブジェクト）の作成
-            stmt = (Statement) con.createStatement();
-            // 4, 5. Select文の実行と結果を格納／代入
+//            con = DriverManager.getConnection(
+//                "jdbc:mysql://localhost/world?useSSL=false&allowPublicKeyRetrieval=true",
+//                "root",
+//                "141592****yt"
+//            );
+//            // 3. DBとやりとりする窓口（Statementオブジェクト）の作成
+//            stmt = (Statement) con.createStatement();
+//            // 4, 5. Select文の実行と結果を格納／代入
             String sql = "SELECT * FROM country LIMIT 50";
-            rs = stmt.executeQuery(sql);
+//            rs = stmt.executeQuery(sql);
+//            
+            try (// DBと接続する
+                    Connection con = DriverManager.getConnection(
+                        "jdbc:mysql://localhost/world?useSSL=false&allowPublicKeyRetrieval=true", "root",
+                        "141592****yt");
+                    // DBとやりとりする窓口（Statementオブジェクト）の作成
+                    Statement stmt = con.createStatement();
+                    // SQLを発行
+                    ResultSet rs = stmt.executeQuery(sql);) {
+                String s = "aaaa";
+                Double.parseDouble(s);
             // 6. 結果を表示する
-            while(rs.next()) {
-                String name = rs.getString("Name");
-                //取得した値を表示
-                System.out.println(name);
+                while(rs.next()) {
+                    String name = rs.getString("Name");
+                    //取得した値を表示
+                    System.out.println(name);
+                }
+            } catch (NumberFormatException e) {
+                System.err.println();
             }
             
-            sql = "update country set Population = 105000 where Code = 'ABW'";
-            int count = stmt.executeUpdate(sql);
-            System.out.println(count);
+//            sql = "update country set Population = 105000 where Code = 'ABW'";
+//            int count = stmt.executeUpdate(sql);
+//            System.out.println(count);
             
        
         } catch (ClassNotFoundException e) {
@@ -52,32 +66,32 @@ public class DbConnectSample01 {
             e.printStackTrace();
         
             // 7. 接続を閉じる
-        }finally {
-         
-            if(rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                    System.err.println("ResultSetを閉じるときにエラーが生じました。");
-                    e.printStackTrace();
-                }
-            }
-            if( stmt != null) {
-                try {
-                    stmt.close();
-                } catch (SQLException e) {
-                    System.err.println("Statementを閉じるときにエラーが発生しました。");
-                    e.printStackTrace();
-                }
-            }
-            if( con != null) {
-                try {
-                    con.close();
-                }catch (SQLException e) {
-                    System.err.println("データベース切断時にエラーが起こりました。");
-                    e.printStackTrace();
-                }
-            }
+//        }finally {
+//         
+//            if(rs != null) {
+//                try {
+//                    rs.close();
+//                } catch (SQLException e) {
+//                    System.err.println("ResultSetを閉じるときにエラーが生じました。");
+//                    e.printStackTrace();
+//                }
+//            }
+//            if( stmt != null) {
+//                try {
+//                    stmt.close();
+//                } catch (SQLException e) {
+//                    System.err.println("Statementを閉じるときにエラーが発生しました。");
+//                    e.printStackTrace();
+//                }
+//            }
+//            if( con != null) {
+//                try {
+//                    con.close();
+//                }catch (SQLException e) {
+//                    System.err.println("データベース切断時にエラーが起こりました。");
+//                    e.printStackTrace();
+//                }
+//            }
         }
         
      
